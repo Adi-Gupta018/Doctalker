17c68b41307353130fd8f64e315009f281a19396
Adi-Gupta018 <adiharsh018@gmail.com>
2024-02-03 11:47:49 +0530

    added components

---
+++ components/Intro.js
@@ -0,0 +1,8 @@
+export default function Intro() {
+	return (
+		<div className={"pb-5 border-b mb-5"}>
+			With <b>DocTalker</b> you can upload any pdf and chat with the pdf to get answers.
+			Just Upload and start chatting!
+		</div>
+	)
+}
