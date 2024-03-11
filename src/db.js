aa5fd8e67dbd2c9e61120036f609a9adf76d6592
Adi-Gupta018 <adiharsh018@gmail.com>
2024-03-11 17:01:14 +0530

    Revert "changed db.js"
    This reverts commit d5e2ee9fe2e1e1e3ec9678466c6033cd6ce55fd5.

--- src/db.js
+++ src/db.js
@@ -1,41 +1,24 @@
-// import mongoose from  'mongoose';
+import mongoose from  'mongoose';
 
-// export const connectDB = async() =>{
-//     if(mongoose.connections[0].readyState){
-//         console.log("existing connection available");
-//         return
-//     }
-
-//     const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@demo.antwhfs.mongodb.net/Doctalker?retryWrites=true&w=majority `;
-
-//     try {
-//         await mongoose.connect(MONGO_URL,{
-//             useUnifiedTopology: true,
-//             useNewUrlParser: true,
-//         });
-//         console.log("Connected to mongo");
-//     } catch (error) {
-//         throw new Error(error);
-        
-//     }
-// }
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
-    if (mongoose.connections[0].readyState) {
-        console.log("Existing connection available");
-        return;
+export const connectDB = async() =>{
+    if(mongoose.connections[0].readyState){
+        console.log("existing connection available");
+        return
     }
 
     const MONGO_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@demo.antwhfs.mongodb.net/Doctalker?retryWrites=true&w=majority `;
 
     try {
-        await mongoose.connect(MONGO_URL);
-        console.log("Connected to Mongo");
+        await mongoose.connect(MONGO_URL,{
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        });
+        console.log("Connected to mongo");
     } catch (error) {
         throw new Error(error);
+        
     }
-};
+}
 
 export const disconnectDB = async() => {
     if(mongoose.connections[0].readyState){
